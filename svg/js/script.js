190d3eaaf541cc17ca2b9ebd2565491b39bea601
Nay Zaw Oo <nayzawoo.me@gmail.com>
2017-02-02 01:13:31 +0630

    Update on 2017-02-02 00:50:51

---
+++ svg/js/script.js
@@ -0,0 +1,62 @@
+var labels = {
+	labelColor: '#fff',
+	fontSize: 18,
+	fontFamily: 'Ours-Unicode',
+	customLabelText: {
+		'MM.SA': "စစ်ကိုင်းတိုင်း",
+		'MM.MD': "မန္တလေးတိုင်း",
+		'MM.YA': "ရန်ကုန်တိုင်း",
+	}
+}
+
+var bubbles = [{
+	name: 'Monywa',
+	radius: 10,
+	fillKey: 'MM.SA',
+	latitude: 22.1065893,
+	longitude: 95.0789498,
+	fillOpacity: 0.3
+}]
+
+// Events
+var done = function(datamap) {
+	datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+		alert(geography.properties.name)
+	})
+}
+
+var map = new Datamap({
+	done: done,
+	responsive: true,
+	fills: {
+		// defaultFill: 'rgb(34, 34, 44)'
+	},
+	element: document.getElementById('map'),
+	scope: 'mmr',
+	setProjection: function(element) {
+		var projection = d3.geo.equirectangular()
+			.center([97, 19])
+			.scale(1800)
+			.translate([element.offsetWidth / 2, element.offsetHeight / 2])
+		var path = d3.geo.path().projection(projection);
+
+		return {
+			path: path,
+			projection: projection
+		}
+	},
+})
+
+map.labels(labels)
+
+map.bubbles(bubbles, {
+	popupTemplate: function(geo, data) {
+		return '<div class="hoverinfo">' + data.name + '</div>'
+	}
+})
+
+// Responsive
+// Pure JavaScript
+window.addEventListener('resize', function() {
+	map.resize()
+})
